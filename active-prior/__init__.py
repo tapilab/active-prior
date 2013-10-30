484e69788edc4b399c05007ad10eb3e1d49c4fab
Aron Culotta <aronwc@gmail.com>
2013-10-30 11:30:48 -0500

    logging

--- active-prior/__init__.py
+++ active-prior/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+import logging.config
+
 __author__ = 'Aron Culotta'
 __email__ = 'aronwc@gmail.com'
 __version__ = '0.1.0'
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
