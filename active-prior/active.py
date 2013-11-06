32deb51e3d981f0a18d6c7113adbd6ca662f3cd4
Aron Culotta <aronwc@gmail.com>
2013-11-06 11:32:33 -0600

    initial pass at class distribution matching oracle

--- active-prior/active.py
+++ active-prior/active.py
@@ -40,6 +40,11 @@ def class_counts(y):
     return [len([yj for yj in y if yj == yi]) for yi in sorted(set(y))]
 
 
+def dist_to_labeled(xi, X):
+    xid = xi.toarray()[0]
+    return np.mean([spatial.distance.cosine(xid, xj.toarray()[0]) for xj in X])
+
+
 class ActiveLearner(object):
     """ Abstract class for an active learning classifier. """
 
@@ -64,7 +69,6 @@ class ActiveLearner(object):
             sys.stdout.write('\riteration %d labeled=%d unlabeled=%d' % (iteri, len(labeled), len(unlabeled)))
             sys.stdout.flush()
             tolabel = self.select_instances(xtrain, labeled, unlabeled)
-            # logging.debug('labeling %s', str(tolabel))
             labeled |= tolabel
             unlabeled -= tolabel
             self.clf.fit(xtrain[list(labeled)], ytrain[list(labeled)])
@@ -138,10 +142,6 @@ class GreedyOracle(ActiveLearner):
     def __init__(self, *args, **kwargs):
         super(GreedyOracle, self).__init__(*args, **kwargs)
 
-    def dist_to_labeled(self, xi, X):
-        xid = xi.toarray()[0]
-        return np.mean([spatial.distance.cosine(xid, xj.toarray()[0]) for xj in X])
-
     def select_instances(self, X, labeled_indices, unlabeled_indices):
         scores = []
         unl = np.array(list(unlabeled_indices))
@@ -158,5 +158,38 @@ class GreedyOracle(ActiveLearner):
         top_indices = np.array(scores).argsort()[::-1][:self.batch_size]
         self.clf.fit(self.xtrain[list(labeled_indices)], self.ytrain[list(labeled_indices)])
         topx = self.xtrain[unl[top_indices[0]]]
-        print '\npredictions=', self.clf.predict_proba(topx), 'truth=', self.ytrain[unl[top_indices[0]]], '#words=', topx.sum(), 'distance=', self.dist_to_labeled(topx, self.xtrain[list(labeled_indices)]), 'acc=', scores[top_indices[0]]
+        print '\npredictions=', self.clf.predict_proba(topx), 'truth=', self.ytrain[unl[top_indices[0]]], '#words=', topx.sum(), 'distance=', dist_to_labeled(topx, self.xtrain[list(labeled_indices)]), 'acc=', scores[top_indices[0]]
+        return set(unl[top_indices])
+
+
+class ClassDistrMatcherOracle(ActiveLearner):
+    """ Select instance such that the predicted class distribution matches that supplied. """
+
+    def __init__(self, *args, **kwargs):
+        super(ClassDistrMatcherOracle, self).__init__(*args, **kwargs)
+
+    def class_distr_match(self, pred, truth):
+        p_cc = 1. * np.array(class_counts(pred)) / len(pred)
+        t_cc = 1. * np.array(class_counts(truth)) / len(pred)
+        s = 1. - spatial.distance.cosine(p_cc, t_cc)
+        return s
+
+    def select_instances(self, X, labeled_indices, unlabeled_indices):
+        scores = []
+        unl = np.array(list(unlabeled_indices))
+        for idx in unl:
+            labeled_tmp = set(labeled_indices)
+            unlabeled_tmp = set(unlabeled_indices)
+            # add to training
+            labeled_tmp |= set([idx])
+            unlabeled_tmp -= set([idx])
+            # train
+            self.clf.fit(self.xtrain[list(labeled_tmp)], self.ytrain[list(labeled_tmp)])
+            # predict on testing
+            scores.append(self.class_distr_match(self.clf.predict(self.xtest), self.ytest))
+        print 'top scores=', sorted(scores, reverse=True)[:10]
+        top_indices = np.array(scores).argsort()[::-1][:self.batch_size]
+        self.clf.fit(self.xtrain[list(labeled_indices)], self.ytrain[list(labeled_indices)])
+        topx = self.xtrain[unl[top_indices[0]]]
+        print '\npredictions=', self.clf.predict_proba(topx), 'truth=', self.ytrain[unl[top_indices[0]]], '#words=', topx.sum(), 'distance=', dist_to_labeled(topx, self.xtrain[list(labeled_indices)]), 'acc=', scores[top_indices[0]]
         return set(unl[top_indices])
